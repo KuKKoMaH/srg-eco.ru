8f537d6bd7b0237e240618092ee13c4d17c6db86
KuKKoMaH <kukkoman@gmail.com>
2019-08-18 12:50:41 +0300

    fixes

--- src/modules/frontSlider/frontSlider.js
+++ src/modules/frontSlider/frontSlider.js
@@ -2,13 +2,15 @@ import { tns } from "tiny-slider/src/tiny-slider";
 
 if ($('.frontSlider__slides').length) {
   const frontSlider = tns({
-    container:  '.frontSlider__slides',
-    items:      1,
-    swipeAngle: false,
-    speed:      400,
-    nav:        false,
-    autoplay:   true,
-    prevButton: '.frontSlider__arrow--left',
-    nextButton: '.frontSlider__arrow--right'
+    container:            '.frontSlider__slides',
+    items:                1,
+    swipeAngle:           false,
+    speed:                400,
+    nav:                  false,
+    autoplay:             true,
+    autoplayButton:       false,
+    autoplayButtonOutput: false,
+    prevButton:           '.frontSlider__arrow--left',
+    nextButton:           '.frontSlider__arrow--right'
   });
 }
