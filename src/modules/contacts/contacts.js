1b2b2b43a7ce7df6c66329bb393b6e2bc6cd9b50
KuKKoMaH <kukkoman@gmail.com>
2020-01-16 22:02:36 +0300

    мелкие фиксы

--- src/modules/contacts/contacts.js
+++ src/modules/contacts/contacts.js
@@ -13,11 +13,11 @@ if ($times.length) {
   const updateTimes = () => {
     const now = new Date();
     const utc = now.getTime() + (now.getTimezoneOffset() * 60000);
-    $times.each((i, el) => {
-      const $el   = $(el);
-      const gmt   = $el.data('gmt');
-      const time  = new Date(utc + (3600000 * gmt));
-      let hours   = time.getHours();
+    $times.each(( i, el ) => {
+      const $el = $(el);
+      const gmt = $el.data('gmt');
+      const time = new Date(utc + (3600000 * gmt));
+      let hours = time.getHours();
       let minutes = time.getMinutes();
       if (hours < 10) hours = '0' + hours;
       if (minutes < 10) minutes = '0' + minutes;
@@ -29,7 +29,7 @@ if ($times.length) {
   updateTimes();
 }
 
-$('.contacts__gallery').each((i, el) => {
+$('.contacts__gallery').each(( i, el ) => {
   let slider;
   const $el = $(el);
 
@@ -64,32 +64,27 @@ $('.contacts__gallery').each((i, el) => {
 const $maps = $('.contacts__map');
 
 if ($maps.length) {
-  scriptLoader(`https://maps.googleapis.com/maps/api/js?key=${window.MAP_API_KEY}`, () => {
-    $maps.each((i, el) => {
-      let { center, zoom } = el.dataset;
+  scriptLoader(`https://api-maps.yandex.ru/2.1/?lang=ru_RU`, () => {
+    ymaps.ready(() => {
+      $maps.each(( i, el ) => {
+        let { center, zoom, address } = el.dataset;
 
-      try {
-        center = JSON.parse(center);
-        zoom   = +zoom;
+        try {
+          center = JSON.parse(center);
+          zoom = +zoom;
 
-        const map = new google.maps.Map(el, {
-          zoom:                     zoom,
-          center:                   new google.maps.LatLng(center[0], center[1]),
-          // styles:                   mapStyles,
-          mapTypeControl:           false,
-          streetViewControl:        false,
-          fullscreenControlOptions: {
-            position: google.maps.ControlPosition.RIGHT_BOTTOM
-          }
-        });
+          const myMap = new ymaps.Map(el, {
+            center:   center,
+            controls: ['fullscreenControl', 'zoomControl'],
+            zoom:     zoom,
+          });
 
-        const marker = new google.maps.Marker({
-          position: new google.maps.LatLng(center[0], center[1]),
-          map:      map,
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    })
+          myMap.geoObjects.add(new ymaps.Placemark(center, { balloonContent: address }));
+
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    });
   });
 }
